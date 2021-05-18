c31e06a3a82af2dc4bff1cb2f83ad19f70e6b20f
Christoph Iserlohn <christoph.iserlohn@innoq.com>
2021-05-18 12:49:36 +0200

    add exercise06-modelout

---
+++ pct/keys/keys.py
@@ -0,0 +1,86 @@
+import enum
+from typing import List, Literal
+
+import fastapi
+from fastapi import Response
+from fastapi.params import Body, Path, Header
+from pydantic import BaseModel, constr
+
+router = fastapi.APIRouter(prefix='/keys')
+
+# Wird vom Test automatisch befüllt. Nicht ändern!
+# Einträge sind Dicts in der Form {id: '<hexstring:32>', origin: '<DE|NL|GB>', timestamp: float}
+all_keys = []
+
+# Wird vom Test automatisch befüllt. Nicht ändern!
+# Einträge sind str in der Form '<hexstring:4>'
+tans = []
+
+COUNTRY_VALIDATION_REGEX = '^(DE|NL|GB)$'
+TAN_VALIDATION_REGEX = '^[0-9A-Fa-f]{4}$'
+ID_VALIDATION_REGEX = '^[0-9A-Fa-f]{32}$'
+
+
+class CountryEnum(str, enum.Enum):
+    DE = 'DE'
+    NL = 'NL'
+    GB = 'GB'
+
+
+class Key(BaseModel):
+    id: constr(regex=ID_VALIDATION_REGEX)
+    # origin: constr(regex=COUNTRY_VALIDATION_REGEX)
+    # origin: CountryEnum
+    origin: Literal['DE', 'NL', 'GB']
+    timestamp: float
+
+# TODO Pydantic Model für die Ausgabe
+#  Erstelle eine Pydantic Model für die Ausgabe. Es enthält keine 'origin', ansonsten ist gleich dem 'Key' Model.
+
+# TODO Pydantic Model für die Ausgabe
+#  gib das neue Model für die Ausgabe zurück statt dem 'Key' Model.
+@router.get('/')
+async def download_all_keys(limit: int = 0):
+    if 0 < limit < len(all_keys):
+        return all_keys[:limit]
+    return all_keys
+
+
+# TODO Pydantic Model für die Ausgabe
+#  gib das neue Model für die Ausgabe zurück statt dem 'Key' Model.
+@router.get('/{country}')
+async def download_country_keys(country: str = Path(..., regex=COUNTRY_VALIDATION_REGEX)):
+    keys = []
+    for key in all_keys:
+        if key['origin'] == country:
+            keys.append(key)
+    return keys
+
+
+@router.post('/', status_code=201)
+async def upload_key(res: Response,
+                     id: str = Body(..., regex=ID_VALIDATION_REGEX),
+                     origin: str = Body(..., regex=COUNTRY_VALIDATION_REGEX),
+                     timestamp: float = Body(...),
+                     x_tan: str = Header(None, regex=TAN_VALIDATION_REGEX)):
+    if x_tan not in tans:
+        res.status_code = 401
+        return
+    else:
+        key = { 'id': id, 'origin': origin, 'timestamp': timestamp}
+        tans.remove(x_tan)
+        all_keys.append(key)
+
+
+@router.post('/bundle', status_code=201)
+async def upload_key_bundle(res: Response,
+                     keys: List[Key],
+                     x_tan: str = Header(None, regex=TAN_VALIDATION_REGEX)):
+    if x_tan not in tans:
+        res.status_code = 401
+        return
+    else:
+        for key in keys:
+            all_keys.append(key)
+    tans.remove(x_tan)
+
--- pct/tests/test_main.py
+++ pct/tests/test_main.py
@@ -1,12 +1,235 @@
+import random
+import secrets
+import time
+import typing
+
+import pytest
 from fastapi.testclient import TestClient
 
 from pct.main import app
 
-import pytest
+KEYS_PREFIX = '/keys'
 
 client = TestClient(app)
 
+
+def create_key(id_length = 16, origins = ['DE', 'GB', 'NL'], timestamp = time.time()):
+    return {
+        'id': secrets.token_hex(id_length),
+        'origin': random.choice(origins),
+        'timestamp': timestamp
+    }
+
+
+def create_test_keys(n: int) -> typing.List[typing.Dict]:
+    return [create_key() for _ in range(n)]
+
+
+def inject_test_keys(test_keys):
+    from pct.keys import keys
+    keys.all_keys += test_keys
+
+
+def delete_injected_test_keys():
+    from pct.keys import keys
+    keys.all_keys.clear()
+
+
+def create_test_tans(n: int) -> typing.List[str]:
+    return [secrets.token_hex(2) for _ in range(n)]
+
+
+def inject_test_tans(test_tans):
+    from pct.keys import keys
+    keys.tans += test_tans
+
+
+def delete_injected_test_tans():
+    from pct.keys import keys
+    keys.tans.clear()
+
+
+@pytest.fixture(scope='function')
+def testdata():
+    N = 25
+    test_tans = create_test_tans(N)
+    inject_test_tans(test_tans)
+    test_keys = create_test_keys(N)
+    inject_test_keys(test_keys)
+    yield test_keys, test_tans
+    delete_injected_test_keys()
+    delete_injected_test_tans()
+
+
 @pytest.mark.helloenterpy
 def test_hello_enterpy():
-    r = client.get('/')
+    r = client.get('/welcome')
     assert r.json() == "Hello enterPY!"
+
+
+@pytest.mark.downloadkeys
+def test_download_keys(testdata):
+    r = client.get(KEYS_PREFIX)
+    assert r.status_code == 200
+    keys = r.json()
+    test_keys, _ = testdata
+    assert keys == [{'id': key['id'], 'timestamp': key['timestamp']} for key in test_keys]
+
+
+@pytest.mark.queryparams
+def test_download_keys_limit(testdata):
+    test_keys, _ = testdata
+    r = client.get(f'{KEYS_PREFIX}?limit=12')
+    assert r.status_code == 200
+    keys = r.json()
+    assert len(keys) == 12
+    test_keys = [{'id': key['id'], 'timestamp': key['timestamp']} for key in test_keys]
+    for key in keys:
+        assert key in test_keys
+
+@pytest.mark.queryparams
+def test_download_keys_invalid_limit():
+    r = client.get(f'{KEYS_PREFIX}?limit=foo')
+    assert r.status_code == 422
+
+
+@pytest.mark.pathparams
+def test_download_keys_country(testdata):
+    test_keys, _ = testdata
+    COUNTRY = 'DE'
+    r = client.get(f'{KEYS_PREFIX}/{COUNTRY}')
+    assert r.status_code == 200
+    keys = r.json()
+    test_keys = [{'id': key['id'], 'timestamp': key['timestamp']} for key in test_keys]
+    for key in keys:
+        assert key in test_keys
+
+
+@pytest.mark.pathparams
+def test_download_keys_invalid_country():
+    COUNTRY = 'DEE'
+    r = client.get(f'{KEYS_PREFIX}/{COUNTRY}')
+    assert r.status_code == 422
+
+
+@pytest.mark.bodyparams
+def test_upload_key():
+    tans = create_test_tans(2)
+    inject_test_tans(tans)
+    key1 = create_key(id_length=16)
+    key2 = create_key(id_length=16)
+    r = client.post(KEYS_PREFIX, json=key1, headers={ 'X-Tan': tans.pop()}, allow_redirects=True)
+    assert r.status_code == 201
+    r = client.post(KEYS_PREFIX, json=key2, headers={ 'X-Tan': tans.pop()}, allow_redirects=True)
+    assert r.status_code == 201
+    r = client.get(KEYS_PREFIX)
+    assert r.status_code == 200
+    keys = r.json()
+    for key in keys:
+        assert key in [{'id': key['id'], 'timestamp': key['timestamp']} for key in [key1, key2]]
+
+
+@pytest.mark.bodyparams
+def test_upload_invalid_key():
+    key1 = create_key(id_length=15)
+    key2 = create_key(16, origins=['DEE'])
+    key3 = create_key(16, origins=['DE'])
+    key3.pop('timestamp')
+    r = client.post(KEYS_PREFIX, json=key1, allow_redirects=True)
+    assert r.status_code == 422
+    r = client.post(KEYS_PREFIX, json=key2, allow_redirects=True)
+    assert r.status_code == 422
+    r = client.post(KEYS_PREFIX, json=key3, allow_redirects=True)
+    assert r.status_code == 422
+
+
+@pytest.mark.headerparams
+def test_upload_tan(testdata: typing.List):
+    keys, tans = testdata
+    for key in keys:
+        r = client.post(KEYS_PREFIX, json=key, headers={'X-Tan': tans.pop()}, allow_redirects=True)
+        assert r.status_code == 201
+
+
+@pytest.mark.headerparams
+def test_upload_without_tan(testdata: typing.List):
+    keys, tans = testdata
+    for key in keys:
+        r = client.post(KEYS_PREFIX, json=key, allow_redirects=True)
+        assert r.status_code == 401
+
+
+@pytest.mark.headerparams
+def test_upload_with_unknown_tan(testdata: typing.List):
+    keys, tans = testdata
+    while True:
+        tan = create_test_tans(1).pop()
+        if tan not in tans:
+            break
+    for key in keys:
+        r = client.post(KEYS_PREFIX, json=key, headers={'X-Tan': tan}, allow_redirects=True)
+        assert r.status_code == 401
+
+
+@pytest.mark.headerparams
+def test_upload_with_invalid_tan(testdata: typing.List):
+    keys, _ = testdata
+    for key in keys:
+        r = client.post(KEYS_PREFIX, json=key, headers={'X-Tan': 'foo'}, allow_redirects=True)
+        assert r.status_code == 422
+
+
+@pytest.mark.headerparams
+def test_upload_with_reused_tan(testdata: typing.List):
+    keys1, tans1 = testdata
+    keys2, tans2 = keys1.copy(), tans1.copy()
+    for key in keys1:
+        r = client.post(KEYS_PREFIX, json=key, headers={'X-Tan': tans1.pop()}, allow_redirects=True)
+        assert r.status_code == 201
+    for key in keys2:
+        r = client.post(KEYS_PREFIX, json=key, headers={'X-Tan': tans2.pop()}, allow_redirects=True)
+        assert r.status_code == 401
+
+@pytest.mark.modelmapping
+def test_upload_bundle():
+    tans = create_test_tans(1)
+    inject_test_tans(tans)
+    test_keys = [create_key() for _ in range(5)]
+    r = client.post(f'{KEYS_PREFIX}/bundle', json=test_keys, headers={'X-Tan': tans.pop()}, allow_redirects=True)
+    assert r.status_code == 201
+    r = client.get(KEYS_PREFIX)
+    assert r.status_code == 200
+    keys = r.json()
+    for key in keys:
+        assert key in test_keys
+
+
+@pytest.mark.modelmapping
+def test_upload_bundle_with_invalid_key():
+    delete_injected_test_keys()
+    tans = create_test_tans(1)
+    inject_test_tans(tans)
+    test_keys = [create_key() for _ in range(5)]
+    test_keys.append(create_key(origins=['NNL']))
+    test_keys.append(create_key(origins=['GBB']))
+    r = client.post(f'{KEYS_PREFIX}/bundle', json=test_keys, headers={'X-Tan': tans.pop()}, allow_redirects=True)
+    assert r.status_code == 422
+    r = client.get(KEYS_PREFIX)
+    assert r.status_code == 200
+    keys = r.json()
+    assert len(keys) == 0
+
+
+@pytest.mark.modelout
+def test_upload_bundle():
+    tans = create_test_tans(1)
+    inject_test_tans(tans)
+    test_keys = [create_key() for _ in range(5)]
+    r = client.post(f'{KEYS_PREFIX}/bundle', json=test_keys, headers={'X-Tan': tans.pop()}, allow_redirects=True)
+    assert r.status_code == 201
+    r = client.get(KEYS_PREFIX)
+    assert r.status_code == 200
+    keys = r.json()
+    test_keys_stripped = [{'id': key['id'], 'timestamp': key['timestamp']} for key in keys ]
+    for key in keys:
+        assert key in test_keys_stripped
