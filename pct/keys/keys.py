569f12c97991c2cf0fd771cfb22603ff73065ebb
Christoph Iserlohn <christoph.iserlohn@innoq.com>
2021-05-19 11:24:20 +0200

    remove solution-03

--- pct/keys/keys.py
+++ pct/keys/keys.py
@@ -21,8 +21,3 @@ async def download_country_keys(country: str = Path(..., regex='^(DE|NL|GB)$')):
             keys.append(key)
     return keys
 
-
-@router.post('/', status_code=201)
-async def upload_key(id: str = Body(..., regex='^[0-9A-Fa-f]{32}$'), origin: str = Body(..., regex='^(DE|NL|GB)$'), timestamp: float = Body(...)):
-    key = { 'id': id, 'origin': origin, 'timestamp': timestamp}
-    all_keys.append(key)
